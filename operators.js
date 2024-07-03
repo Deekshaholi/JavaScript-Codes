66f9855c810e6d291ceb847dc03c969c04b2b8e4
Deeksha Holi <128277483+Deekshaholi@users.noreply.github.com>
2024-07-03 13:12:27 +0530

    Add files via upload

--- operators.js
+++ operators.js
@@ -1,14 +1,23 @@
-/*let x = '10',
-    y = '20';
-let result = x + y;
+// equal to operator
+console.log("Equal to: 2 == 2 is", 2 == 2);
 
-console.log(result);*/
+// not equal operator
+console.log("Not equal to: 3 != 3 is", 3 != 3);
 
+// strictly equal to operator
+console.log("Strictly equal to: 2 === '2' is", 2 === '2');
 
+// strictly not equal to operator
+console.log("Strictly not equal to: 2 !== '2' is", 2 !== '2');
 
+// greater than operator
+console.log("Greater than: 3 > 3 is", 3 > 3);
 
-let a = 10, 
-    b = 20; 
+// less than operator
+console.log("Less than: 2 > 2 is", 2 > 2);
 
-console.log(a >= b);  // false
-console.log(a == 10); // true
+// greater than or equal to operator
+console.log("Greater than or equal to: 3 >= 3 is", 3 >= 3);
+
+// less than or equal to operator
+console.log("Less than or equal to: 2 <= 2 is", 2 <= 2);
